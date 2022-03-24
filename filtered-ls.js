6c9974da7a7aee53d49ae4f2f3e1ea4ac11511e8
assaad ahmed <assaaddouz12@gmail.com>
2022-03-24 10:33:49 +0100

    first commit

---
+++ filtered-ls.js
@@ -0,0 +1,13 @@
+var fs=require('fs');
+var path=require('path');
+var filepath=process.argv[2];
+var ext="."+process.argv[3];
+
+fs.readdir(filepath,function(err,list) {
+  if (err) throw err;
+  for (var i=0;i<list.length-1;i++) {
+    if (("."+path.extname(list[i]))==ext) {
+      console.log(list[i]);
+    }
+  }
+});
